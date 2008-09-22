b0e67d2db594799244e6aede1847b248454111d3
Michael Johnston <lastobelus@mac.com>
2008-09-22 16:33:41 -0700

    added rest_client.rb

---
+++ lib/casclient/rest_client.rb
@@ -0,0 +1,27 @@
+module CASClient
+  class RestClient < Client
+    attr_writer :restful_url
+    
+    def configure(conf)
+      @restful_url = conf[:login_url]
+      super
+    end
+   
+    def restful_url
+      @restful_url || (cas_base_url + "/cas/v1/tickets")
+    end
+    
+    def get_ticket_granting_ticket_resource(credentials)
+      resp = submit_data_to_cas(restful_url, credentials.stringify_keys, '&')
+      header = resp.to_hash
+      header['location']
+    end
+    
+    def get_service_ticket(credentials, service_url)      
+      tgt_resource = get_ticket_granting_ticket_resource(credentials)
+      data = credentials.merge(:service => CGI.escape(service_url)).stringify_keys
+      resp = submit_data_to_cas(restful_url, data, '&')
+      
+    end
+  end
+end
